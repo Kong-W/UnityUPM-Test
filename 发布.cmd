88411a82336f75636cd5ae650b113e6adb9730b7
kongwei <835475119@qq.com>
2021-05-14 10:51:27 +0800

    发布设置

---
+++ 发布.cmd
@@ -0,0 +1,16 @@
+::设置模块名字,发布的时候 改路径，名字 版本
+SET ToolName=upm-yourmodule
+::设置模块版本
+SET ToolVersion=yourmodule-1.0.0
+::设置模块源路径
+SET ToolAssetPath=Assets/
+
+::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
+git subtree split -P %ToolAssetPath% --branch %ToolName%
+:: 在ToolName分支设置标签ToolVersion节点
+git tag %ToolVersion% %ToolName%
+
+:: 推送到远端
+git push origin %ToolName% %ToolVersion%
+git push origin %ToolName%
+pause
